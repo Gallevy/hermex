358120e0921f2a340f316b38c8e77c54e3e1d8ef
Gal <gallevy3313@gmail.com>
2025-11-29 18:09:41 +0200

    Fix mismatch between zed and biome

--- release.config.mjs
+++ release.config.mjs
@@ -3,20 +3,20 @@
 export default {
   branches: [
     {
-      name: "main",
-      prerelease: "alpha",
+      name: 'main',
+      prerelease: 'alpha',
     },
   ],
   plugins: [
-    "@semantic-release/commit-analyzer",
-    "@semantic-release/release-notes-generator",
-    "@semantic-release/changelog",
-    ["@semantic-release/npm", { npmPublish: true }],
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    ['@semantic-release/npm', { npmPublish: true }],
     [
-      "@semantic-release/git",
+      '@semantic-release/git',
       {
-        assets: ["package.json", "CHANGELOG.md"],
-        message: "chore(release): ${nextRelease.version} [skip ci]",
+        assets: ['package.json', 'CHANGELOG.md'],
+        message: 'chore(release): ${nextRelease.version} [skip ci]',
       },
     ],
   ],
